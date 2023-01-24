5541e65f03d36c27333c13d40972dd831be53319
Andre Freitas <andre@sharpcomputing.io>
2023-01-24 00:33:00 -0300

    auth middleware config

--- src/modules/main/main.module.ts
+++ src/modules/main/main.module.ts
@@ -3,11 +3,18 @@ import { ConfigModule } from '@nestjs/config';
 import SubscriberRabbitModule from '../rabbitmq/subscriber/subscriber.module';
 import PublisherRabbitModule from '../rabbitmq/publisher/publisher.module';
 import TestModule from '../test/test.module';
+import AuthModule from '../auth/auth.module';
 import MainService from './main.service';
 import MainController from './main.controller';
 
 @Module({
-    imports: [ConfigModule.forRoot(), SubscriberRabbitModule, PublisherRabbitModule, TestModule],
+    imports: [
+        ConfigModule.forRoot(),
+        SubscriberRabbitModule,
+        PublisherRabbitModule,
+        TestModule,
+        AuthModule,
+    ],
     controllers: [MainController],
     providers: [MainService],
 })
